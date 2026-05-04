6febbc149255a55e2087bf69b2eb374f233ef150
Nicolas091109 <nicol.cuevasl@duocuc.cl>
2026-05-04 13:24:01 -0400

    se agregaron las bases de los 3 microservicios

---
+++ ms-reporting/ms-reporting/src/main/java/cl/duoc/sanosysalvos/ms_reporting/MsReportingApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.sanosysalvos.ms_reporting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsReportingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsReportingApplication.class, args);
+	}
+
+}
